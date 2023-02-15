0c6fddb036f4d0112da1878462092a234664dc26
Ayush Aman <nympseudo259@gmail.com>
2023-02-15 12:40:15 +0530

    Upload the Dataset and the Recommed File

---
+++ bestReads/recommend.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+#Importing the dataset
+books = pd.read_csv('../dataset/Books.csv')
+ratings = pd.read_csv('../dataset/Ratings.csv')
+users = pd.read_csv('../dataset/Users.csv')
+
+#Popularity based recommendation system
+
